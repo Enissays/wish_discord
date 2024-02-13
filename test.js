17fcfb895c5de7f961b394da4807be34635b381f
Enissays <yas.ben.dev@gmail.com>
2024-02-13 20:57:00 +0100

    Fix user ID in u_data.set() function

--- test.js
+++ test.js
@@ -7,4 +7,4 @@ const cards = require("./utilitary/cards.json");
 
 const u_data = new Enmap({name: "chocolates"});
 
-u_data.set("849936690915442698",{get:0,left:3,user:interaction.user.id});
+u_data.set("849936690915442698",{get:0,left:3,user:"849936690915442698"});
