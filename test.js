7a9fd6e567aee2d6fe75b8ecd00cafd3776f8702
Enissays <yas.ben.dev@gmail.com>
2024-02-13 20:56:30 +0100

    Update Enmap data for user

--- test.js
+++ test.js
@@ -2,11 +2,9 @@ var Enmap = require('enmap');
 
 
 
-const u_data = new Enmap({name: "points"});
 const ranks = require("./utilitary/fn_ranks.js");
 const cards = require("./utilitary/cards.json");
 
-var data = ranks.getRanks({id:"849936690915442698", username:"Eni"}, u_data);
-data.coins = 50;
-u_data.set("849936690915442698", data);
-console.log(u_data.get("849936690915442698"));
+const u_data = new Enmap({name: "chocolates"});
+
+u_data.set("849936690915442698",{get:0,left:3,user:interaction.user.id});
