d78e44d71f4df7e349c94ffb5c9de4a5cba055cd
Enissays <yas.ben.dev@gmail.com>
2024-02-14 13:56:00 +0100

    Delete user data for ID 690531678086496337

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ const u_data = new Enmap({name: "points"});
 const ranks = require("./utilitary/fn_ranks.js");
 const cards = require("./utilitary/cards.json");
 
+u_data.delete("690531678086496337");
 var data = ranks.getRanks({id:"690531678086496337", username:"Maggie"}, u_data);
 data.cards = Object.keys(cards).filter((card) => !["robinet_double_chaud_froid", "alex_merde_ramadan", "eni_strong", "sidi_khalkhal", "tg_tom_2"].includes(card));
 data.rank.level = 4;
