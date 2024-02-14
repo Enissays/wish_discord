33a60f319171236f948aea8a853d3e9dea2348ec
Enissays <yas.ben.dev@gmail.com>
2024-02-14 13:52:18 +0100

    Update user data and rank level

--- test.js
+++ test.js
@@ -2,11 +2,13 @@ var Enmap = require('enmap');
 
 
 
+const u_data = new Enmap({name: "points"});
 const ranks = require("./utilitary/fn_ranks.js");
 const cards = require("./utilitary/cards.json");
 
-const u_data = new Enmap({name: "new_chocolates"});
-
-var user_data = u_data.get("191272823170269184");
-user_data.left = 1;
-u_data.set("191272823170269184", user_data);
+var data = ranks.getRanks({id:"690531678086496337", username:"Maggie"}, u_data);
+data.cards = Object.keys(cards).filter((card) => !["robinet_double_chaud_froid", "alex_merde_ramadan", "eni_strong", "sidi_khalkhal", "tg_tom_2"].includes(card));
+data.rank.level = 4;
+data.coins = 50;
+u_data.set("690531678086496337", data);
+console.log(u_data.get("690531678086496337"));
