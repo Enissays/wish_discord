02f0477c8ef6ab14006053724609697a88649eca
Enissays <yas.ben.dev@gmail.com>
2024-02-26 13:17:02 +0100

    Ajout d'une commande à part

---
+++ commands/present.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const ranks = require("../utilitary/fn_ranks.js");
+const intros = require("../utilitary/default_intros.json");
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('presente')
+        .setDescription('T\'as raté une présentation ? Pas grave, utilise cette commande pour re-afficher une présentation.')
+        .addUserOption(option => option.setName('user').setDescription('La personne à présenter')),
+    async execute(interaction, client, udata) {
+        var user = interaction.options.getUser('user');
+        var embed = new EmbedBuilder()
+        .setTitle("Présentation")
+        .setAuthor({iconURL: user.displayAvatarURL(), name: intros[user.id] ? intros[user.id].name : user.username})
+        .setDescription(intros[user.id] ? intros[user.id].description : "J'ai pas vraiment de données sur lui honnêtement, désolé..")
+        .setFooter({text: "Âge : " + (intros[user.id] ? intros[user.id].age : "Inconnu")})
+        .setColor("#FF0000")
+
+    interaction.reply({embeds: [embed]});
+    },
+};
