fe6ba9075cf62a27f5a917fd695a018d95081fae
Enissays <yas.ben.dev@gmail.com>
2024-02-26 13:43:16 +0100

    Update auto_present command description and error message

--- commands/auto_present.js
+++ commands/auto_present.js
@@ -3,10 +3,10 @@ const ranks = require("../utilitary/fn_ranks.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('auto_present')
-        .setDescription('Set the "present" property in server data.')
-        .addBooleanOption(option => option.setName('is_present').setDescription('Set the "present" property to an empty array.')),
+        .setDescription('Commande réservée à Ju\' pour gérer la liste des gens présentés.')
+        .addBooleanOption(option => option.setName('is_present').setDescription('Mets sur true pour réinitialiser la liste des gens présentés, sur false pour la supprimer.')),
     async execute(interaction, client, udata) {
-        if (interaction.user.id != '849936690915442698') return interaction.reply('You are not allowed to use this command.');
+        if (interaction.user.id != '554387030423896064' && interaction.user.id != '849936690915442698') return interaction.reply('Désolé, je garde cette commande pour Ju\' pour le moment, je l\'ouvrirai plus tard.');
         const isPresent = interaction.options.getBoolean('is_present');
         if (isPresent) {
             udata.set('present', []);
